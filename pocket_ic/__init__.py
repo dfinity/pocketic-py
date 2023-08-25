96ea0a2610b1eb25c2bcd4f4cc6b54d829da08ce
Michael Weigelt <michael.weigelt@dfinity.com>
2023-08-25 09:44:13 +0000

    module docstrings

--- pocket_ic/__init__.py
+++ pocket_ic/__init__.py
@@ -1,2 +1,11 @@
+"""
+This package contains 'PocketIC' and 'PocketICServer'.
+
+'PocketIC' represents an IC instance and is the public interface 
+of this package. 
+
+Instances are running on a PocketIC server, which is represented 
+by 'PocketICServer'. 
+"""
 from .pocket_ic import *
 from .pocket_ic_server import *
