157731595a355de775c6550382d03ea5815128ad
Bas van Dijk <bas@dfinity.org>
2023-08-29 14:22:35 +0000

    Use poetry and poetry2nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "PocketIC Python Libary";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     pocket-ic-darwin-gz = {
       url = "https://download.dfinity.systems/ic/80bcca3b3e9e79bd07af2747e9cffb0e50c6b868/openssl-static-binaries/x86_64-darwin/pocket-ic.gz";
@@ -17,30 +17,61 @@
   outputs = { self, nixpkgs, flake-utils, pocket-ic-darwin-gz, pocket-ic-linux-gz }:
     flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ] (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-                (python-final: python-prev: {
-                  antlr4-python3-runtime = python-prev.antlr4-python3-runtime.override { antlr4 = final.antlr4_9; };
-                  waiter = python-final.callPackage ./waiter.nix { };
-                  ic-py = python-final.callPackage ./ic-py.nix { };
-                  pocketic-py = python-final.callPackage ./pocketic-py.nix { };
-                })
-              ];
-              pocket-ic = final.callPackage ./pocket-ic.nix {
-                inherit pocket-ic-darwin-gz pocket-ic-linux-gz;
-              };
-            })
-          ];
+        pkgs = import nixpkgs { inherit system; };
+
+        pocket-ic = pkgs.callPackage ./pocket-ic.nix {
+          inherit pocket-ic-darwin-gz pocket-ic-linux-gz;
         };
-        pocketic-py = pkgs.python3.pkgs.pocketic-py;
+
+        overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend
+          (_final: prev:
+            let
+              addSetuptools = drv:
+                drv.overridePythonAttrs
+                  (
+                    old: {
+                      buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ];
+                    }
+                  );
+            in
+            {
+              waiter = addSetuptools prev.waiter;
+              ic-py = addSetuptools prev.ic-py;
+            });
+
+        pocketic-py = pkgs.poetry2nix.mkPoetryApplication {
+          inherit projectDir overrides;
+        };
+
+        pocketic-py-env = pkgs.poetry2nix.mkPoetryEnv {
+          inherit projectDir overrides;
+        };
+
+        projectDir = pkgs.lib.cleanSourceWith {
+          name = "pocketic-py-src";
+          src = ./.;
+          # Filter out files not needed by the Python package:
+          filter = path: _type:
+            ! (pkgs.lib.hasSuffix ".nix" path ||
+              pkgs.lib.hasSuffix "flake.lock" path ||
+              pkgs.lib.hasSuffix ".envrc" path ||
+              pkgs.lib.hasSuffix ".github" path ||
+              pkgs.lib.hasSuffix ".gitignore" path);
+        };
+
       in
       {
         packages.default = pocketic-py;
 
         packages.pocket-ic = pkgs.pocket-ic;
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.poetry
+            pocketic-py-env
+            pocket-ic
+          ];
+        };
 
         # so that we can format .nix code using: nix fmt
         formatter = pkgs.nixpkgs-fmt;
