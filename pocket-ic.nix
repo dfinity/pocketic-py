56c8a9a3fbc4c68ceeb0f36f10e761ad94602728
Bas van Dijk <bas@dfinity.org>
2023-08-29 08:28:36 +0000

    pocket-ic.nix: refactor

--- pocket-ic.nix
+++ pocket-ic.nix
@@ -11,22 +11,20 @@ stdenv.mkDerivation {
     chmod +x $out/bin/pocket-ic
   '';
   preFixup =
-    if stdenv.isDarwin
-    then ''
-      # TODO: install_name_tool?
-    ''
-    else
-      let
-        libPath = lib.makeLibraryPath [
-          stdenv.cc.cc.lib # libstdc++.so.6
-        ];
-      in
-      ''
-        patchelf \
-          --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-          --set-rpath "${libPath}" \
-          $out/bin/pocket-ic
-      '';
+    lib.optionalString (!stdenv.isDarwin)
+      (
+        let
+          libPath = lib.makeLibraryPath [
+            stdenv.cc.cc.lib # libstdc++.so.6
+          ];
+        in
+        ''
+          patchelf \
+            --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+            --set-rpath "${libPath}" \
+            $out/bin/pocket-ic
+        ''
+      );
   doInstallCheck = true;
   preInstallCheck = ''
     $out/bin/pocket-ic --help
