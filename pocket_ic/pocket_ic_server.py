b8266cf0f55bd01e4f9bdd76ab8072d2063487fa
Elias Datler <elias.datler@dfinity.org>
2023-08-30 20:32:51 +0200

    refactor and better error handling

--- pocket_ic/pocket_ic_server.py
+++ pocket_ic/pocket_ic_server.py
@@ -4,11 +4,11 @@ This module contains the 'PocketICServer', which starts or discovers a PocketIC 
 
 import os
 import time
-from typing import List
+from typing import List, Any
 from tempfile import gettempdir
 import requests
 
-POCKET_IC_BIN = "pocket-ic"
+BINARY_NAME = "pocket-ic"
 
 
 class PocketICServer:
@@ -29,7 +29,7 @@ class PocketICServer:
     def __init__(self) -> None:
         # Attempt to start the PocketIC server if it's not already running.
         pid = os.getpid()
-        os.system(f"{POCKET_IC_BIN} --pid {pid} &")
+        os.system(f"{BINARY_NAME} --pid {pid} &")
         self.url = self._get_url(pid)
         self.request_client = requests.session()
 
@@ -54,14 +54,27 @@ class PocketICServer:
 
         return f"http://127.0.0.1:{port}"
 
+    def new_instance(self) -> str:
+        """Creates a new PocketIC instance.
+
+        Returns:
+            str: the new instance ID
+        """
+        url = f"{self.url}/instances"
+        response = self.request_client.post(url)
+        self._check_response(response)
+        return response.text
+
     def list_instances(self) -> List[str]:
         """Lists the currently running instances on the PocketIC Server.
 
         Returns:
             List[str]: a list of instance names
         """
-        response = self.request_client.get(f"{self.url}/instances").text
-        return response.split(", ") if response else []
+        url = f"{self.url}/instances"
+        response = self.request_client.get(url)
+        self._check_response(response)
+        return response.text.split(", ") if response.text else []
 
     def delete_instance(self, instance_id: str):
         """Deletes an instance from the PocketIC Server.
@@ -69,4 +82,35 @@ class PocketICServer:
         Args:
             instance_id (str): the ID of the instance to delete
         """
-        self.request_client.delete(f"{self.url}/instances/{instance_id}/delete")
+        url = f"{self.url}/instances/{instance_id}/delete"
+        response = self.request_client.delete(url)
+        self._check_response(response)
+
+    def send_request_to_instance(self, instance_id: str, body: Any) -> Any:
+        """Send a request to a specific PocketIC instance.
+
+        Args:
+            instance_id (str): the instance ID
+            body (Any): the payload for the IC request
+
+        Returns:
+            Any: a JSON encoded result, if the request contains a response
+        """
+        url = f"{self.url}/instances/{instance_id}"
+        response = self.request_client.post(url, json=body)
+        self._check_response(response)
+        return response.json()
+
+    def _check_response(self, response):
+        """Checks the response from the PocketIC server.
+
+        Args:
+            response (Response): the response from a call made with `requests`
+
+        Raises:
+            ConnectionError: raised on response status codes != 200
+        """
+        if response.status_code != 200:
+            raise ConnectionError(
+                f'PocketIC Server returned status code {response.status_code}: "{response.reason}"'
+            )
