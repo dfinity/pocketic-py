02ba9abe820254a286835436c5904081ab8c2b28
Elias Datler <46360620+fxgst@users.noreply.github.com>
2023-11-08 14:46:33 +0100

    feat(VER-2585): Add `get/set_stable_memory` functions (#49)

--- pocket_ic/pocket_ic_server.py
+++ pocket_ic/pocket_ic_server.py
@@ -51,27 +51,6 @@ where $platform is 'x86_64-linux' for Linux and 'x86_64-darwin' for Intel/rosett
         self.url = self._get_url(pid)
         self.request_client = requests.session()
 
-    def _get_url(self, pid: int) -> str:
-        tmp_dir = gettempdir()
-        ready_file_path = f"{tmp_dir}/pocket_ic_{pid}.ready"
-        port_file_path = f"{tmp_dir}/pocket_ic_{pid}.port"
-
-        stop_at = time.time() + 10  # Wait for the ready file for 10 seconds
-
-        while not os.path.exists(ready_file_path):
-            if time.time() < stop_at:
-                time.sleep(0.1)  # 100ms
-            else:
-                raise TimeoutError("PocketIC failed to start")
-
-        if os.path.isfile(ready_file_path):
-            with open(port_file_path, "r", encoding="utf-8") as port_file:
-                port = port_file.readline().strip()
-        else:
-            raise ValueError(f"{ready_file_path} is not a file!")
-
-        return f"http://127.0.0.1:{port}"
-
     def new_instance(self) -> str:
         """Creates a new PocketIC instance.
 
@@ -115,18 +94,56 @@ where $platform is 'x86_64-linux' for Linux and 'x86_64-darwin' for Intel/rosett
         response = self.request_client.post(url, json=body, headers=HEADERS)
         return self._check_response(response)
 
-    def _check_response(self, response):
-        """Checks the response from the PocketIC server.
+    def set_blob_store_entry(self, blob: bytes, compression) -> str:
+        """Sets a blob store entry.
 
         Args:
-            response (Response): the response from a call made with `requests`
+            blob (bytes): the blob to set
+            compression (str/None): "gzip" or None
 
-        Raises:
-            ConnectionError: raised on response status codes not in [200, 201, 202]
+        Returns:
+            str: the blob store key
         """
-        if response.status_code not in [200, 201, 202]:
-            raise ConnectionError(
-                f'PocketIC Server returned status code {response.status_code}: "{response.reason}"'
-            )
+        url = f"{self.url}/blobstore"
+        if compression is None:
+            response = self.request_client.post(url, data=blob, headers=HEADERS)
+        elif compression == "gzip":
+            headers = HEADERS | {"Content-Encoding": "gzip"}
+            response = self.request_client.post(url, data=blob, headers=headers)
+        else:
+            raise ValueError(f'only "gzip" compression is supported')
+        
+        self._check_status_code(response)
+        return response.text
+    
+    def _get_url(self, pid: int) -> str:
+        tmp_dir = gettempdir()
+        ready_file_path = f"{tmp_dir}/pocket_ic_{pid}.ready"
+        port_file_path = f"{tmp_dir}/pocket_ic_{pid}.port"
+
+        stop_at = time.time() + 10  # Wait for the ready file for 10 seconds
+
+        while not os.path.exists(ready_file_path):
+            if time.time() < stop_at:
+                time.sleep(0.1)  # 100ms
+            else:
+                raise TimeoutError("PocketIC failed to start")
+
+        if os.path.isfile(ready_file_path):
+            with open(port_file_path, "r", encoding="utf-8") as port_file:
+                port = port_file.readline().strip()
+        else:
+            raise ValueError(f"{ready_file_path} is not a file!")
+
+        return f"http://127.0.0.1:{port}"
+
+    def _check_response(self, response):
+        self._check_status_code(response)
         res_json = response.json()
         return res_json
+    
+    def _check_status_code(self, response):
+        if response.status_code not in [200, 201, 202]:
+            raise ConnectionError(
+                f'PocketIC server returned status code {response.status_code}: "{response.reason}"'
+            )
