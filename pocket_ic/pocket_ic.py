5e84d835456b4038408ce16561905a8aebe41a66
Elias Datler <elias.datler@dfinity.org>
2025-04-25 17:31:26 +0200

    Update PocketIC

--- pocket_ic/pocket_ic.py
+++ pocket_ic/pocket_ic.py
@@ -1,143 +1,13 @@
 """
-This module contains 'PocketIC', which is the main interface we expose to a test author.
-It also contains 'SubnetConfig' and 'SubnetKind', which are used to configure the
-subnets of a PocketIC instance.
+This module contains 'PocketIC', which is the main interface exposed to the test author.
 """
 
 import base64
 import ic
-from enum import Enum
 from ic.candid import Types
 from typing import Optional, Any
 from pocket_ic.pocket_ic_server import PocketICServer
-
-
-class SubnetKind(Enum):
-    """The kind of subnet."""
-
-    APPLICATION = "Application"
-    BITCOIN = "Bitcoin"
-    FIDUCIARY = "Fiduciary"
-    II = "II"
-    NNS = "NNS"
-    SNS = "SNS"
-    SYSTEM = "System"
-    VERIFIED_APPLICATION = "VerifiedApplication"
-
-
-class SubnetConfig:
-    """The configuration of subnets for a PocketIC instance."""
-
-    def __init__(
-        self,
-        application=0,
-        bitcoin=False,
-        fiduciary=False,
-        ii=False,
-        nns=False,
-        sns=False,
-        system=0,
-        verified_application=0,
-    ) -> None:
-        new = {"state_config": "New", "instruction_config": "Production"}
-        self.application = [new] * application
-        self.bitcoin = new if bitcoin else None
-        self.fiduciary = new if fiduciary else None
-        self.ii = new if ii else None
-        self.nns = new if nns else None
-        self.sns = new if sns else None
-        self.system = [new] * system
-        self.verified_application = [new] * verified_application
-
-    def __repr__(self) -> str:
-        return f"SubnetConfigSet(application={self.application}, bitcoin={self.bitcoin}, fiduciary={self.fiduciary}, ii={self.ii}, nns={self.nns}, sns={self.sns}, system={self.system}, verified_application={self.verified_application})"
-
-    def validate(self) -> None:
-        """Validates the subnet configuration.
-
-        Raises:
-            ValueError: if no subnet is configured
-        """
-        if not (
-            self.bitcoin
-            or self.fiduciary
-            or self.ii
-            or self.nns
-            or self.sns
-            or self.system
-            or self.application
-            or self.verified_application
-        ):
-            raise ValueError("At least one subnet must be configured.")
-
-    def add_subnet_with_state(
-        self, subnet_type: SubnetKind, state_dir_path: str, subnet_id: ic.Principal
-    ):
-        """Add a subnet with state loaded form the given state directory.
-        Note that the provided path must be accessible for the PocketIC server process.
-
-        `state_dir` should point to a directory which is expected to have the following structure:
-
-        state_dir/
-         |-- backups
-         |-- checkpoints
-         |-- diverged_checkpoints
-         |-- diverged_state_markers
-         |-- fs_tmp
-         |-- page_deltas
-         |-- states_metadata.pbuf
-         |-- tip
-         `-- tmp
-
-        `subnet_id` should be the subnet ID of the subnet in the state to be loaded"""
-
-        raw_subnet_id_bytes = base64.b64encode(
-            subnet_id.bytes
-        )  # convert bytes to base64 bytes
-        raw_subnet_id = raw_subnet_id_bytes.decode()  # convert bytes to str
-
-        new_from_path = {
-            "state_config": {
-                "FromPath": [state_dir_path, {"subnet_id": raw_subnet_id}]
-            },
-            "instruction_config": "Production",
-        }
-
-        match subnet_type:
-            case SubnetKind.APPLICATION:
-                self.application.append(new_from_path)
-            case SubnetKind.BITCOIN:
-                self.bitcoin = new_from_path
-            case SubnetKind.FIDUCIARY:
-                self.fiduciary = new_from_path
-            case SubnetKind.II:
-                self.ii = new_from_path
-            case SubnetKind.NNS:
-                self.nns = new_from_path
-            case SubnetKind.SNS:
-                self.sns = new_from_path
-            case SubnetKind.SYSTEM:
-                self.system.append(new_from_path)
-            case SubnetKind.VERIFIED_APPLICATION:
-                self.verified_application.append(new_from_path)
-
-    def _json(self) -> dict:
-        return {
-            "subnet_config_set": {
-                "application": self.application,
-                "bitcoin": self.bitcoin,
-                "fiduciary": self.fiduciary,
-                "ii": self.ii,
-                "nns": self.nns,
-                "sns": self.sns,
-                "system": self.system,
-                "verified_application": self.verified_application,
-            },
-            "state_dir": None,
-            "nonmainnet_features": False,
-            "log_level": None,
-            "bitcoind_addr": None,
-        }
+from pocket_ic.subnet_config import SubnetConfig, SubnetKind
 
 
 class PocketIC:
@@ -549,19 +419,21 @@ class PocketIC:
             "payload": base64.b64encode(payload).decode(),
         }
 
-        submit_ingress_message = self._instance_post("update/submit_ingress_message", body)
+        submit_ingress_message = self._instance_post(
+            "update/submit_ingress_message", body
+        )
         ok = self._get_ok(submit_ingress_message)
         result = self._instance_post("update/await_ingress_message", ok)
         return self._get_ok_data(result)
-    
+
     def _get_ok(self, request_result):
         if "Ok" in request_result:
             return request_result["Ok"]
         if "Err" in request_result:
-            err = request_result['Err']
-            reject_code = err['reject_code']
-            reject_message = err['reject_message']
-            error_code = err['error_code']
+            err = request_result["Err"]
+            reject_code = err["reject_code"]
+            reject_message = err["reject_message"]
+            error_code = err["error_code"]
             msg = f"PocketIC returned a rejection error: reject code {reject_code}, reject message {reject_message}, error code {error_code}"
             raise ValueError(msg)
         raise ValueError(f"Malformed response: {request_result}")
